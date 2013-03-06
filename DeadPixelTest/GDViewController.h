40447fdda10fd0f2b0be467ffc755f0759a74695
Larry Tin <i@goodow.com>
2013-03-06 15:53:03 +0800

    Initial Commit

---
+++ DeadPixelTest/GDViewController.h
@@ -0,0 +1,13 @@
+//
+//  GDViewController.h
+//  DeadPixelTest
+//
+//  Created by dev on 13-3-6.
+//  Copyright (c) 2013年 Larry Tin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GDViewController : UIViewController
+
+@end
