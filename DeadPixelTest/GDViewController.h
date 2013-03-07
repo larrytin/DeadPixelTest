23353e8b09262eb31cedd825e8a3ea5590eecdda
Larry Tin <i@goodow.com>
2013-03-07 14:34:49 +0800

    Make it works.

--- DeadPixelTest/GDViewController.h
+++ DeadPixelTest/GDViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface GDViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *tip;
 
 @end
--- DeadPixelTest/GDViewController.m
+++ DeadPixelTest/GDViewController.m
@@ -10,6 +10,8 @@
 
 @interface GDViewController ()
 
+@property(strong, nonatomic) NSArray *colors;
+
 @end
 
 @implementation GDViewController
@@ -18,12 +20,34 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  self.colors = @[[UIColor colorWithRed:0 green:0 blue:0 alpha:1],
+                  [UIColor colorWithRed:1 green:1 blue:1 alpha:1],
+                  [UIColor colorWithRed:1 green:0 blue:0 alpha:1],
+                  [UIColor colorWithRed:0 green:1 blue:0 alpha:1],
+                  [UIColor colorWithRed:0 green:0 blue:1 alpha:1],
+                  ];
+  UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(switchBackground)];
+  [self.view addGestureRecognizer:tapGestureRecognizer];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)switchBackground {
+  int idx = [self.colors indexOfObject:self.view.backgroundColor];
+  self.view.backgroundColor = [self.colors objectAtIndex: ++idx % self.colors.count];
+}
+
+
+-(void)viewWillAppear:(BOOL)animated {
+  [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(dismissTip) userInfo:nil repeats:NO];
+}
+
+-(void)dismissTip{
+  self.tip.hidden = YES;
 }
 
 @end
